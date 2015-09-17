92eee733e07cf94ab116467865058f2002447dce
Spencer Christensen <schriste@adobe.com>
2015-09-17 13:12:29 -0600

    Added Postgres backup script. again.

---
+++ pgsql/backup-pg-databases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "$(date) Backing up Postgres databases"
+
+echo "Dumping global settings and users"
+pg_dumpall --global > postgres-global-dump.sql
+gzip postgres-global-dump.sql
+
+echo "Dumping each database..."
+for db in `psql -qt -c "select datname from pg_database where datname not like 'template%' and datname not like 'information_schema'"`
+do
+    echo $db
+    pg_dump $db > $db.dump.sql
+    gzip $db.dump.sql
+done
+
+echo "$(date) Done"
