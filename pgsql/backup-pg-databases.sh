5446e22011fa36a4ee4019a62b7f0f1dd0c4eda9
Spencer Christensen <schriste@adobe.com>
2015-09-22 10:29:35 -0600

    Added -f (force) flag to backup-pg-databases.sh

--- pgsql/backup-pg-databases.sh
+++ pgsql/backup-pg-databases.sh
@@ -4,14 +4,14 @@ echo "$(date) Backing up Postgres databases"
 
 echo "Dumping global settings and users"
 pg_dumpall --global > postgres-global-dump.sql
-gzip postgres-global-dump.sql
+gzip -f postgres-global-dump.sql
 
 echo "Dumping each database..."
 for db in `psql -qt -c "select datname from pg_database where datname not like 'template%' and datname not like 'information_schema'"`
 do
     echo $db
     pg_dump $db > $db.dump.sql
-    gzip $db.dump.sql
+    gzip -f $db.dump.sql
 done
 
 echo "$(date) Done"
