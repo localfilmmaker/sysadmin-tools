c3fb92170494ddd01ad6b2a9564ebbbf5c2f1646
Spencer Christensen <schriste@adobe.com>
2015-10-12 11:27:47 -0600

    Updated backup-pg-databases.sh to set the directory for backups to be the same dir as where the script lives.

--- pgsql/backup-pg-databases.sh
+++ pgsql/backup-pg-databases.sh
@@ -2,16 +2,19 @@
 
 echo "$(date) Backing up Postgres databases"
 
+backups_dir=`dirname $0`
+echo "Backups dir: $backups_dir/"
+
 echo "Dumping global settings and users"
-pg_dumpall --global > postgres-global-dump.sql
-gzip -f postgres-global-dump.sql
+pg_dumpall --global > $backups_dir/postgres-global-dump.sql
+gzip -f $backups_dir/postgres-global-dump.sql
 
 echo "Dumping each database..."
 for db in `psql -qt -c "select datname from pg_database where datname not like 'template%' and datname not like 'information_schema'"`
 do
     echo $db
-    pg_dump $db > $db.dump.sql
-    gzip -f $db.dump.sql
+    pg_dump $db > $backups_dir/$db.dump.sql
+    gzip -f $backups_dir/$db.dump.sql
 done
 
 echo "$(date) Done"
